32376bdaf9680be77a5d9976937b2ce818fb5852
Paravi Khullar <66310260+paravikhullar@users.noreply.github.com>
2020-06-12 12:42:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var fixedRect, movingRect;
+
+function setup() 
+
+{ 
+  createCanvas(1200,800); 
+
+  fixedRect = createSprite(600, 400, 50, 80); 
+  fixedRect.shapeColor = "green"; 
+  fixedRect.debug = true;
+  fixedRect.velocityX=-3 
+
+  movingRect = createSprite(300,400,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true; 
+  movingRect.velocityX=3
+
+}
+
+  function draw() 
+  { 
+    background(255); 
+    /*movingRect.x = World.mouseX; 
+    movingRect.y = World.mouseY;*/
+     if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && 
+        fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 &&
+        movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 &&
+        fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2)
+      {
+         movingRect.velocityX=movingRect.velocityX*(-1);
+         fixedRect.velocityX=fixedRect.velocityX*(-1);
+
+       movingRect.shapeColor = "red";
+       fixedRect.shapeColor = "red";
+      }
+     else{
+       movingRect.shapeColor = "green";
+       fixedRect.shapeColor = "green";
+     }
+    drawSprites();
+    createEdgeSprites();
+
+
+  }
